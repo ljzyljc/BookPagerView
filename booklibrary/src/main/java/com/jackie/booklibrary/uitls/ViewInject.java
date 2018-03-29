0458c2cc97de1321a2fc2413c5cf704adbab2dc6
JackieLin <feifei5292190@163.com>
2018-03-29 18:03:49 +0800

    添加注释

--- booklibrary/src/main/java/com/jackie/booklibrary/uitls/ViewInject.java
+++ booklibrary/src/main/java/com/jackie/booklibrary/uitls/ViewInject.java
@@ -1,12 +1,21 @@
 package com.jackie.booklibrary.uitls;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
  * Created by Jackie on 2018/3/29.
  */
+//  RetentionPolicy的类型有以下几种
+//
+//            1.SOURCE:在源文件中有效（即源文件保留）
+//          　2.CLASS:在class文件中有效（即class保留）
+//          　3.RUNTIME:在运行时有效（即运行时保留）
+
 @Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ViewInject {
 //    因为value这个名字是默认的，如果我们定义为value，那么注解的时候可以省略
     int value();
