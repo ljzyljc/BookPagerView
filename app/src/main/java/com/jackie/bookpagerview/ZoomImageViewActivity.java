c867bd136abf7d31a607e9c651491da42d502927
JackieLin <feifei5292190@163.com>
2018-04-11 15:53:43 +0800

    图片缩放移动器

---
+++ app/src/main/java/com/jackie/bookpagerview/ZoomImageViewActivity.java
@@ -0,0 +1,18 @@
+package com.jackie.bookpagerview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.ImageView;
+
+/**
+ * Created by Jackie on 2018/4/11.
+ */
+
+public class ZoomImageViewActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zoomimage);
+    }
+}
